0d8a3e038b747b966fbf6edcf019e4d5fac1ad3b
hoquangnam45 <hoquangnam45@gmail.com>
2025-09-30 13:00:50 +0700

    fix bug

--- auth-service/src/main/java/com/hoquangnam45/cdc/image/app/auth/AuthApplication.java
+++ auth-service/src/main/java/com/hoquangnam45/cdc/image/app/auth/AuthApplication.java
@@ -2,8 +2,10 @@ package com.hoquangnam45.cdc.image.app.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.hoquangnam45.cdc.image.app.common", "com.hoquangnam45.cdc.image.app.auth"})
 public class AuthApplication {
 
     public static void main(String[] args) {
--- image-service/src/main/java/com/hoquangnam45/cdc/image/app/image/ImageApplication.java
+++ image-service/src/main/java/com/hoquangnam45/cdc/image/app/image/ImageApplication.java
@@ -2,8 +2,10 @@ package com.hoquangnam45.cdc.image.app.image;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.hoquangnam45.cdc.image.app.common", "com.hoquangnam45.cdc.image.app.image"})
 public class ImageApplication {
 
     public static void main(String[] args) {
